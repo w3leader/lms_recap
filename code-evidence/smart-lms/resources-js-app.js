d016c474f43750bf888df48b92733532c1cb783d
Wathit Phonlit <wathith.p@gmail.com>
2026-06-19 17:06:33 +0700

    Remove weak Smart LMS JS scaffold evidence

--- code-evidence/smart-lms/resources-js-app.js
+++
@@ -1,9 +0,0 @@
-require('./bootstrap');
-
-window.Vue = require('vue').default;
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-const app = new Vue({
-    el: '#app',
-});
